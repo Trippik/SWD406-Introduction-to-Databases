98cac8c2693d2dc53aefc8f4b82e433fdd1ea7f9
Trippik <43576788+Trippik@users.noreply.github.com>
2019-04-30 20:48:34 +0100

    Create 08_Driver_Insert.sql

---
+++ ORACLE_Scripts/Part_2/02_New_Movie_Data/00_Segmented_Scripts/08_Driver_Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Driver (Character_id, vehicle_id) VALUES (67, 79);
+INSERT INTO Driver (Character_id, vehicle_id) VALUES (68, 8);
+INSERT INTO Driver (Character_id, vehicle_id) VALUES (70, 80);
