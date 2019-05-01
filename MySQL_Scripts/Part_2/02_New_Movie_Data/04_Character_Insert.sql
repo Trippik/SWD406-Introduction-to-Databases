aa91b565b5717f290b521f8557032c726d2a72a3
Trippik <43576788+Trippik@users.noreply.github.com>
2019-05-01 12:42:43 +0100

    Create 04_Character_Insert.sql

---
+++ MySQL_Scripts/Part_2/02_New_Movie_Data/04_Character_Insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO Star_Wars_Character(id, planet_id, species_id, Character_name, height, mass, hair_color, skin_color, eye_color, birth_year, gender) 
+VALUES (66, 3, 1, 'Poe Dameron', 172, 80, 4, 50, 4, '2ABY', 'Male');
+
+INSERT INTO Star_Wars_Character(id, planet_id, species_id, Character_name, height, mass, hair_color, skin_color, eye_color, birth_year, gender) 
+VALUES (67, 61, 1, 'Rey', 170, 54, 4, 11, 25, '15ABY', 'Female');
+
+INSERT INTO Star_Wars_Character(id, planet_id, species_id, Character_name, height, mass, hair_color, skin_color, eye_color, birth_year, gender) 
+VALUES (68, 32, 1, 'Kylo Ren', 189, 54, 4, 11, 4, '5ABY', 'Male');
+
+INSERT INTO Star_Wars_Character(id, planet_id, species_id, Character_name, height, mass, hair_color, skin_color, eye_color, birth_year, gender) 
+VALUES (69, 32, 1, 'Ben Solo', 189, 54, 4, 11, 4, '5ABY', 'Male');
+
+INSERT INTO Star_Wars_Character(id, species_id, Character_name, height, mass, hair_color, skin_color, eye_color, birth_year, gender) 
+VALUES (70, 1, 'Finn', 178, 73, 4, 17, 4, '11ABY', 'Male');
