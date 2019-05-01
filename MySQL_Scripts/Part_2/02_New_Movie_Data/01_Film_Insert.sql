664c88501cc4de3ee2d620f806a1758275d2aaf5
Trippik <43576788+Trippik@users.noreply.github.com>
2019-05-01 12:40:41 +0100

    Create 01_Film_Insert.sql

---
+++ MySQL_Scripts/Part_2/02_New_Movie_Data/01_Film_Insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO Film (id, producer, title, release_date, episode_id, director, opening_crawl)
+VALUES (8, 	'Kathleen Kennedy, Ram Bergman', 'The Last Jedi', '2017-12-09', 8, 'Rian Johnson', 
+'The FIRST ORDER reigns.
+Having decimated the peaceful
+Republic, Supreme Leader Snoke
+now deploys the merciless
+legions to seize military
+control of the galaxy.
+
+Only General Leia Organas
+band of RESISTANCE fighters
+stand against the rising
+tyranny, certain that Jedi
+Master Luke Skywalker will
+return and restore a spark of
+hope to the fight.
+
+But the Resistance has been
+exposed. As the First Order
+speeds toward the rebel base,
+the brave heroes mount a
+desperate escape....');
