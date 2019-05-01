86c7dc8059f7de50b8a44240c4a1c468359a18cd
Trippik <43576788+Trippik@users.noreply.github.com>
2019-05-01 12:45:34 +0100

    Create 08_Driver_Insert.sql

---
+++ MySQL_Scripts/Part_2/02_New_Movie_Data/08_Driver_Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Driver (Star_Wars_Character_id, vehicle_id) VALUES (67, 79);
+INSERT INTO Driver (Star_Wars_Character_id, vehicle_id) VALUES (68, 8);
+INSERT INTO Driver (Star_Wars_Character_id, vehicle_id) VALUES (70, 80);
