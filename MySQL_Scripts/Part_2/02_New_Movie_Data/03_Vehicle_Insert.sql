9aafbc3eb23dbbb3b32f162f99fd5fcc581d20eb
Trippik <43576788+Trippik@users.noreply.github.com>
2019-05-01 12:41:50 +0100

    Create 03_Vehicle_Insert.sql

---
+++ MySQL_Scripts/Part_2/02_New_Movie_Data/03_Vehicle_Insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO Vehicle (id, vehicle_class, Vehicle_name) VALUES (78, 'Speeder', "Rey's Speeder");
+INSERT INTO Vehicle (id, vehicle_class, Vehicle_name) VALUES (79, 'Speeder', 'Ski Speeder');
